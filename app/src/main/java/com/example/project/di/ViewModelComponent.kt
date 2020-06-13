e4aa88d09c521bf8ba7b1dc74cac23bba8a87858
AssemAabdirashova <asemsuper2001@mail.ru>
2020-06-13 19:31:20 +0600

    di for preferences added

--- app/src/main/java/com/example/project/di/ViewModelComponent.kt
+++ app/src/main/java/com/example/project/di/ViewModelComponent.kt
@@ -2,9 +2,12 @@ package com.example.project.di
 
 import com.example.project.viewmodel.ListVewModel
 import dagger.Component
+import javax.inject.Singleton
 
-@Component(modules = [ApiModule::class])
+@Singleton
+@Component(modules = [ApiModule::class, AppModule::class, PrefsModule::class])
 interface ViewModelComponent {
+
     fun inject(viewModel: ListVewModel)
 
 }
